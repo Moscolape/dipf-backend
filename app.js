e59c296ee1aad7f2b51ce4022b71f8ceacbb9676
Chukwunenye Moses <chunkums088@gmail.com>
2025-05-31 22:53:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -10,29 +10,37 @@ const memberRoutes = require("./routes/membersRoutes");
 const app = express();
 app.use(express.json());
 
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174", "https://deimperialphilanthropicfamily.org"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "https://deimperialphilanthropicfamily.org",
+];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, origin);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["POST", "GET", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, origin);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+    const PORT = process.env.PORT || 8080;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
   .catch((err) => console.log("MongoDB Connection Error:", err));
 
-app.use('/api/v1', jambScholarshipRoutes);
-app.use('/api/v1', userRoutes);
-app.use('/api/v1', analyticsRoutes);
-app.use('/api/v1', memberRoutes);
-
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use("/api/v1", jambScholarshipRoutes);
+app.use("/api/v1", userRoutes);
+app.use("/api/v1", analyticsRoutes);
+app.use("/api/v1", memberRoutes);
